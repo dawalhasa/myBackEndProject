0befdaf739a96c048fb4c33cd02c920d6d3fc9c8
Dawalhasa <dawalhasa@Dawalhasas-MacBook-Pro.local>
2023-10-20 09:10:02 +0530

    mongoDB connection is done.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
-const router = require('./controller/router')
+const router = require('./controllers/router.js')
+const mongoose = require('mongoose');
+const connectDB = require('./model/dbCon')
+const PORT = process.env.PORT || 5000
 
+
+// connect to MongoDB
+connectDB()
 // express app
 const app = express()
+
 
 // view engine
 app.set('view engine', 'ejs')
@@ -12,21 +19,28 @@ app.set('view engine', 'ejs')
 app.use(morgan('tiny'))
 
 
-// stactic middleware
+// middleware stactic file and photos
 app.use(express.static('public'))
 
 app.get("/")
+app.get("home")
 app.get("ASK")
 app.get("about")
 app.get("product")
-app.get("contact") 
+app.get("contact")
 app.get("table")
 app.get("login")
 
+app.use(router);
 
-app.use(router)
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    // Listening PORT No.
+    app.listen(PORT, ()=> {
+        console.log(`server is runing on port No. ${PORT}`)
+    })
+})
 
 app.use((req, res) => {
     res.status(404).render('404')
 })
-app.listen(5000)
---
+++ model/dbCon.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+require('dotenv').config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MY_DATABASE, {
+            useUnifiedTopology: "true",
+            useNewUrlParser: "true"
+        })
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = connectDB
