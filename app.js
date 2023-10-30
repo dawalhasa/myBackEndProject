3830b2491b4a75abd290e5c9abb42cbef334dfbd
Dawalhasa <dawalhasa@Dawalhasas-MacBook-Pro.local>
2023-10-30 18:01:03 +0530

    Database is connect to the application and I need to config the schema in next step

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ const router = require('./router/router.js');
 const path = require('path');
 require('dotenv').config();
 const expressLayouts = require('express-ejs-layouts')
-// const mongoose = require('mongoose');
-// const connectDB = require('./model/dbCon')
+const mongoose = require('mongoose');
+const connectDB = require('./model/dbCon')
 const PORT = process.env.PORT || 4000
 
 
 // connect to MongoDB
-// connectDB()
-// express app
+connectDB();
+
+// Instantiate express app 
 const app = express();
 
 
 // view engine
-// app.use(expressLayouts)
-// app.set('layout', './layouts/allPage.ejs');
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/allPages.ejs')
 app.use(expressLayouts);
+
 //middleware & log page
 app.use(morgan('dev'))
 
@@ -33,12 +33,12 @@ app.use('/css', express.static(path.join(__dirname, 'css/style.css')))
 
 app.use(router);
 
-// mongoose.connection.once('open', () => {
-//     console.log('Connected to MongoDB');
-//     // Listening PORT No.
-// })
-app.listen(PORT, () => {
-    console.log(`server is runing on port No. ${PORT}`)
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    // Listening PORT No.
+    app.listen(PORT, () => {
+        console.log(`server is runing on port No. ${PORT}`)
+    })
 })
 
 app.use((req, res) => {
